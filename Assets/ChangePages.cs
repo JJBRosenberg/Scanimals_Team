8ccde20f742841d0db22bf744e511e56142cae76
ReportMex9 <jjrosenberg122@gmail.com>
2021-11-18 21:22:24 +0100

    Extra pages
    Guardian page, student page

--- Assets/ChangePages.cs
+++ Assets/ChangePages.cs
@@ -6,11 +6,41 @@ public class ChangePages : MonoBehaviour
 {
     [SerializeField] private GameObject TapToScanPage;
     [SerializeField] private GameObject ChildrenOrGuardianPage;
-
+    [SerializeField] private GameObject ChildrenPage;
+    [SerializeField] private GameObject GuardianPage;
+    [SerializeField] private GameObject firstInfoButton;
+    [SerializeField] private GameObject secondInfoButton;
+    [SerializeField] private GameObject thirdInfoButton;
 
     public void TapToScanButton()
     {
         TapToScanPage.gameObject.SetActive(false);
         ChildrenOrGuardianPage.gameObject.SetActive(true);
+    }
+
+    public void TapForChildren()
+    {
+        ChildrenOrGuardianPage.gameObject.SetActive(false);
+        ChildrenPage.gameObject.SetActive(true);
+    }
+    public void TapForGuardian()
+    {
+        ChildrenOrGuardianPage.gameObject.SetActive(false);
+        GuardianPage.gameObject.SetActive(true);
+    }
+
+    public void firstInfo()
+    {
+        firstInfoButton.gameObject.SetActive(true);
+    }
+
+    public void secondInfo()
+    {
+        secondInfoButton.gameObject.SetActive(true);
+    }
+
+    public void thirdInfo()
+    {
+        thirdInfoButton.gameObject.SetActive(true);
     }
 }

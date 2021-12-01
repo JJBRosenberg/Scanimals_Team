138f4ce1a1a9b657e9a294ae011a3fdf88987b04
jj <jjrosenberg122@gmail.com>
2021-12-01 10:59:33 +0100

    MapPAge

--- Assets/Scripts/changeMap.cs
+++ Assets/Scripts/changeMap.cs
@@ -5,29 +5,29 @@ using UnityEngine.UI;
 
 public class changeMap : MonoBehaviour
 {
-    [SerializeField] RawImage thisMap;
-    [SerializeField] private Texture maps;
-    [SerializeField] private Texture maps1;
-    [SerializeField] private Texture maps2;
+    [SerializeField] Image thisMap;
+    [SerializeField] private Sprite maps;
+    [SerializeField] private Sprite maps1;
+    [SerializeField] private Sprite maps2;
     //[SerializeField] private RawImage maps1;
     //[SerializeField] private RawImage maps2;
     // Start is called before the first frame update
-    public TMPro.TMP_Dropdown myDrop;
+    public Dropdown myDrop;
     public void Update()
     {
         if (myDrop.value == 0)
         {
-            thisMap.texture = maps;
+            thisMap.sprite = maps;
         }
 
         if (myDrop.value == 1)
         {
-            thisMap.texture = maps1;
+            thisMap.sprite = maps1;
         }
 
         if (myDrop.value == 2)
         {
-            thisMap.texture = maps2;
+            thisMap.sprite = maps2;
         }
     }
 }
